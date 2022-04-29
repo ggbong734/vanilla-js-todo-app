4cf5a27fc323d8a10fab66ad53e3ddb68b2e1ba3
Gerry Bong <ggbong734@gmail.com>
2022-04-28 18:51:27 -0700

    completed app and added gitignore

---
+++ src/index.ts
@@ -0,0 +1,53 @@
+console.log("ready");
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".todo").forEach((container) => todo(container));
+});
+
+function todo(container: Element) {
+  container.innerHTML = `
+  <h2>ToDo</h2>
+  <ul></ul>
+  <h3>0 Done</h3>
+  <form>
+    <input type="text" name="text"/>
+    <button type="submit">Add</button>
+  </form>
+  `;
+
+  const form = container.querySelector<HTMLFormElement>("form");
+  const list = container.querySelector<HTMLUListElement>("ul");
+  const done = container.querySelector<HTMLHeadingElement>("h3");
+
+  form.addEventListener("submit", (event: Event) => {
+    event.preventDefault(); // prevent it from submitting the form when enter is pressed
+    const input = form.elements["text"] as HTMLInputElement;
+    // can also select using form.querySelector("input[name='text']");
+    addItem(input.value);
+    form.reset();
+  });
+
+  function addItem(itemText: string) {
+    const item = document.createElement("li");
+    item.appendChild(document.createTextNode(itemText));
+    const check = document.createElement("input");
+    check.type = "checkbox";
+    check.addEventListener("change", () => recount());
+    item.appendChild(check); // there is also prependChild if we want it to show above
+    list.appendChild(item);
+
+    const button = document.createElement("button");
+    button.textContent = "Delete";
+    button.addEventListener("click", () => {
+      list.removeChild(item);
+      recount();
+    });
+    item.appendChild(button);
+
+    list.appendChild(item);
+  }
+
+  function recount() {
+    const count = list.querySelectorAll("input:checked").length;
+    done.textContent = `${count} Done`;
+  }
+}
